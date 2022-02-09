04de066715b35b308d02009efb753d78c24e9ea0
susobhankr <susobhan3114@gmail.com>
2022-02-09 21:33:15 +0530

    Added: UC1 Initialisation

---
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.gamblingsimulator;
+
+public class GamblingSimulator {
+	public static int Stake = 100;
+	public static int Bet = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulator");
+		System.out.println("Total Stake is: " + Stake);
+		System.out.println("Bet per Game: " + Bet);
+	}
+}
